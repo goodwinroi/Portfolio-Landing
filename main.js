71b6c09fe4f0a285a3a097395fd980272e21ebae
goodwinroi <63928988+goodwinroi@users.noreply.github.com>
2020-08-27 19:03:10 -0600

    Update main.js

--- main.js
+++ main.js
@@ -1,64 +1,62 @@
 
-  // Your web app's Firebase configuration
-  var firebaseConfig = {
-    apiKey: "AIzaSyBFRaDrmqnxumLU5MEj31g3hB-7v7HRgIs",
-    authDomain: "portfolio-landing-form.firebaseapp.com",
-    databaseURL: "https://portfolio-landing-form.firebaseio.com",
-    projectId: "portfolio-landing-form",
-    storageBucket: "portfolio-landing-form.appspot.com",
-    messagingSenderId: "218550048723",
-    appId: "1:218550048723:web:e5593cae1c6c6314a78126",
-    measurementId: "G-J2N51TX19E"
-  };
+ // Your web app's Firebase configuration
+var firebaseConfig = {
+  apiKey: "AIzaSyBFRaDrmqnxumLU5MEj31g3hB-7v7HRgIs",
+  authDomain: "portfolio-landing-form.firebaseapp.com",
+  databaseURL: "https://portfolio-landing-form.firebaseio.com",
+  projectId: "portfolio-landing-form",
+  storageBucket: "portfolio-landing-form.appspot.com",
+  messagingSenderId: "218550048723",
+  appId: "1:218550048723:web:e5593cae1c6c6314a78126",
+  measurementId: "G-J2N51TX19E"
+};
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+
+
+
   // Initialize Firebase
-  firebase.initializeApp(firebaseConfig);
-  firebase.initializeApp(firebase.databaseConfig);
+
+  // Reference messages collection
+  var messagesRef = firebase.database().ref('messages');
+
+  // Listen for form submit
+  document.getElementById('contactForm').addEventListener('submit', submitForm);
+
+  // Submit form
+  function submitForm(e){
+    e.preventDefault();
+
+    // Get values
+    var name = getInputVal('name');
+    var company = getInputVal('company');
+    var email = getInputVal('email');
+    var phone = getInputVal('phone');
+    var message = getInputVal('message');
+
+    // Save message
+    saveMessage(name, company, email, phone, message);
+
+    // Show alert
 
 
-  // Initialize Firebase
+    // Clear form
+    document.getElementById('contactForm').reset();
+  }
 
+  // Function to get get form values
+  function getInputVal(id){
+    return document.getElementById(id).value;
+  }
 
-
-// Reference messages collection
-// this is the code that does not work
-var messagesRef = firebase.database() .ref('messages');
-
-// Listen for form submit
-
-document.getElementById('contactForm').addEventListener('submit',submitForm);
-//submit form
-function submitForm(e){
-  e.preventDefault();
-
-
-// Get values
-var name = getInputVal('name');
-var company = getInputVal('company');
-var email = getInputVal('email');
-var phone = getInputVal('phone');
-var message = getInputVal('message');
-
-//save message
-saveMessage (name, company, email, phone, message);
-
-}
-
-
-
-// Function to get get form values
-function getInputVal(id){
-return document.getElementById(id).value;
-
-}
-
-//save message to firebase
-
-function saveMessage(name, company, email, phone, message){var newMessageRef = messagesRef.push();
-newMessageRef.set({
- name: name,
- company: company,
- email: email,
- phone: phone,
- message: message
-}) ;
-}
+  // Save message to firebase
+  function saveMessage(name, company, email, phone, message){
+    var newMessageRef = messagesRef.push();
+    newMessageRef.set({
+      name: name,
+      company:company,
+      email:email,
+      phone:phone,
+      message:message
+    });
+  }
